4412d51240a495e968dad42d02a17c465d3c208d
Igor Souza <88636711+igorsouzaccruz@users.noreply.github.com>
2023-01-05 16:06:26 -0300

    relacionamento entre as entidades

--- APICatalogo/Program.cs
+++ APICatalogo/Program.cs
@@ -2,7 +2,7 @@ using APICatalogo.Context;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
-
+ 
 // Add services to the container.
 
 builder.Services.AddControllers();
