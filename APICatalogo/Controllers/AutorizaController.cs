bb9d5a616b2843a5491ad13d350ce7a0eae75e41
Igor Souza <88636711+igorsouzaccruz@users.noreply.github.com>
2023-03-01 16:44:46 -0300

    implementando o token JWT

--- APICatalogo/Controllers/AutorizaController.cs
+++ APICatalogo/Controllers/AutorizaController.cs
@@ -2,6 +2,12 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.JsonWebTokens;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using JwtRegisteredClaimNames = System.IdentityModel.Tokens.Jwt.JwtRegisteredClaimNames;
 
 namespace APICatalogo.Controllers
 {
@@ -11,11 +17,14 @@ namespace APICatalogo.Controllers
     {
         private readonly UserManager<IdentityUser> _userManager;
         private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly IConfiguration _configuration;
 
-        public AutorizaController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
+        public AutorizaController(UserManager<IdentityUser> userManager, 
+            SignInManager<IdentityUser> signInManager, IConfiguration configuration)
         {
             _userManager = userManager;
             _signInManager = signInManager;
+            _configuration = configuration;
         }
 
         [HttpGet]
@@ -46,7 +55,7 @@ namespace APICatalogo.Controllers
             }
 
             await _signInManager.SignInAsync(user, false);
-            return Ok();
+            return Ok(GeraToken(model));
         }
 
         [HttpPost("login")]
@@ -64,13 +73,51 @@ namespace APICatalogo.Controllers
 
             if(result.Succeeded)
             {
-                return Ok();
+                return Ok(GeraToken(userInfo));
             }
             else
             {
                 ModelState.AddModelError(string.Empty, "Login Inválido...");
                 return BadRequest(ModelState);
             }
+        }
+
+        private UsuarioToken GeraToken(UsuarioDTO userInfo)
+        {
+            //define declarações do usuário
+            var claims = new[]
+            {
+                new Claim(JwtRegisteredClaimNames.UniqueName, userInfo.Email),
+                new Claim("meuPet", "pipoca"),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+            };
+
+            //gera uma chave com base em um algoritmo simetrico
+            var key = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(_configuration["Jwt:key"]));
+            //gera a assinatura digital do token usando o algoritmo Hmac e a chave privada
+            var credenciais = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            //tempo de expiração do token.
+            var expiracao = _configuration["TokenConfiguration:ExpireHours"];
+            var expiration = DateTime.UtcNow.AddHours(double.Parse(expiracao));
+
+            //classe que representa um token JWT e gera o token
+            JwtSecurityToken token = new JwtSecurityToken(
+                issuer: _configuration["TokenConfiguration:Issuer"],
+                audience: _configuration["TokenConfiguration:Audience"],
+                claims: claims,
+                expires: expiration,
+                signingCredentials: credenciais);
+
+            //retorna os dados com o token e informacoes
+            return new UsuarioToken()
+            {
+                Authenticated = true,
+                Token = new JwtSecurityTokenHandler().WriteToken(token),
+                Expiration = expiration,
+                Message = "Token JWT OK"
+            };
         }
     }
 }
