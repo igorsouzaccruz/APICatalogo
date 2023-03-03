35b8f4584e4a53619b1c328388abe274bf170611
Igor Souza <88636711+igorsouzaccruz@users.noreply.github.com>
2023-03-03 13:26:41 -0300

    versionamento de api

--- APICatalogo/Program.cs
+++ APICatalogo/Program.cs
@@ -4,6 +4,7 @@ using APICatalogo.Repository;
 using AutoMapper;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.Versioning;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
@@ -78,6 +79,14 @@ builder.Services.AddAuthentication(
             IssuerSigningKey = new SymmetricSecurityKey(
                 Encoding.UTF8.GetBytes(builder.Configuration["Jwt:key"]))
         });
+
+builder.Services.AddApiVersioning(options =>
+{
+    options.AssumeDefaultVersionWhenUnspecified = true;
+    options.DefaultApiVersion = new Microsoft.AspNetCore.Mvc.ApiVersion(1, 0);
+    options.ReportApiVersions= true;
+    options.ApiVersionReader = new HeaderApiVersionReader("x-api-version");
+});
 
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 
